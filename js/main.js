ab5e201f4ab57a0ecaf3dce338b85e93b41da4e1
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2023-06-24 17:04:55 +0500

    price section and light box styled and added js

--- js/main.js
+++ js/main.js
@@ -84,3 +84,50 @@ for (let i = 0; i < totalFilterBtn; i++) {
         }
     })
 }
+/*===== Lightbox =====*/
+const lightbox = document.querySelector(".lightbox"),
+    lightboxImg = lightbox.querySelector(".lightbox-img"),
+    lightboxClose = lightbox.querySelector(".lightbox-close"),
+    lightboxText = lightbox.querySelector(".caption-text"),
+    lightboxCounter = lightbox.querySelector(".caption-counter");
+let itemIndex = 0;
+for (let i = 0; i < totalportfolioItem; i++) {
+    PortfolioItems[i].addEventListener("click", function () {
+        itemIndex = i;
+        changeItem();
+        toggleLightbox();
+    })
+}
+function nextItem() {
+    if (itemIndex == totalportfolioItem - 1) {
+        itemIndex = 0;
+    }
+    else {
+        itemIndex++
+    }
+    changeItem();
+}
+function prevItem() {
+    if (itemIndex == 0) {
+        itemIndex = totalportfolioItem - 1;
+    }
+    else {
+        itemIndex--
+    }
+    changeItem();
+}
+function toggleLightbox() {
+    lightbox.classList.toggle("open");
+}
+function changeItem() {
+    imgSrc = PortfolioItems[itemIndex].querySelector(".portfolio-img img").getAttribute("src");
+    lightboxImg.src = imgSrc;
+    lightboxText.innerHTML = PortfolioItems[itemIndex].querySelector("h4").innerHTML;
+    lightboxCounter.innerHTML = (itemIndex + 1) + " of " + totalportfolioItem;
+}
+// close lightbox
+lightbox.addEventListener("click", function (event) {
+    if (event.target === lightboxClose || event.target === lightbox) {
+        toggleLightbox()
+    }
+})
